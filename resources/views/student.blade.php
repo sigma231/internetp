4689f71045eb5ef9233d4fb4f6b5fb3961d0924e
sigma231 <cliffshawn109@gmail.com>
2018-06-12 11:57:25 +0300

    final commit

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
 <body>
+    <a href="/">Home Page</a>
+    <div class="input-group col-md-4">
+        
  <form method="post" action="/add_student">
  {{ csrf_field() }}
-     <label for="student">Student Number</label>
-     <input type="number" name="student_number">
-     <label for="student">Student name</label>
-     <input type="text" name="student_name">
-     <label for="address">Address</label>
-     <input type="text" name="address">
-     <label for="date_of_birth">Date Of Birth</label>
-     <input type="date" name="date_of_birth"> 
-     <input type="submit">
+     <label for="student" class="form-control">Student Number</label>
+     <input type="number" name="student_number" class="form-control">
+     <label for="student" class="form-control">Student name</label>
+     <input type="text" name="student_name" class="form-control">
+     <label for="address" class="form-control">Address</label>
+     <input type="text" name="address" class="form-control">
+     <label for="date_of_birth" class="form-control"> Date Of Birth</label>
+     <input type="date" name="date_of_birth" class="form-control"> 
+     <input type="submit" class="btn btn-default">
      </form>
+     </div>
 </body>
